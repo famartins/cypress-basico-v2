14ddfbc99e4a7673eac72848b9a9a7116f0d3cec
famartins <fabricio.mesteves@gmail.com>
2024-09-24 19:43:30 -0300

    feat aula 2 ex1

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Central de atendimento ao cliente TAT", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
+
+  it("verifica o titulo da aplicação", () => {
+    cy.title().should("be.equal", "Central de atendimento ao Cliente TAT");
+  });
+
+  it.only("preenche os campos obrigatórios e envia o formulário", () => {
+    cy.get("#firstName").click().type("Fabricio");
+    cy.get("#lastName").click().type("Martins Esteves");
+    cy.get("#email").click().type("fabricio.mnesteves@gmail.com");
+    cy.get("#open-text-area")
+      .click()
+      .type("Estou com um problema na minha máquina");
+    cy.get('.button[type="submit"]').click();
+    cy.get(".success").should("be.visible")
+  });
+});
